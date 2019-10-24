d51861de0017a1d456e1f0feb18dbb4304f00201
oops <oops>
2019-10-24 15:02:10 -0600

    Adding Timer

---
+++ Unity/Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Timer : MonoBehaviour
+{
+    //public Text timerText;
+    //float startTime;
+    //float stopTime; 
+    //public Button start;
+    //public Button stop; 
+
+    void Start()
+    {
+        //start.onClick.AddListener(StartTimer);
+        //stop.onClick.AddListener(StartTimer);
+    }
+
+    public void StartTimer()
+    {
+        print("START"); 
+       /* startTime = Time.time;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+        */
+    }
+
+    public void StopTimer()
+    {
+        print("STOP");
+        /*
+        timerText.text = stop
+        */
+    }
+
+}
