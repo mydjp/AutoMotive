9c3ad785af87b0696d73e55198645aa900fb1cf8
oops <oops>
2019-10-29 15:28:02 -0600

    Added Functioning Timer with Start, Stop, Reset Buttons

--- Unity/Assets/Scripts/Timer.cs
+++ Unity/Assets/Scripts/Timer.cs
@@ -5,38 +5,57 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    //public Text timerText;
-    //float startTime;
-    //float stopTime; 
-    //public Button start;
-    //public Button stop; 
+    public Text timerMinutes;
+    public Text timerSeconds;
+    public Text timerMilliSeconds;
 
+    private float startTime;
+    private float stopTime;
+    private float timerTime;
+
+    private bool isRunning = false;
+
+    // Start is called before the first frame update
     void Start()
     {
-        //start.onClick.AddListener(StartTimer);
-        //stop.onClick.AddListener(StartTimer);
+        TimerReset();
     }
 
-    public void StartTimer()
+    public void TimerStart()
     {
-        print("START"); 
-       /* startTime = Time.time;
-
-        float t = Time.time - startTime;
-
-        string minutes = ((int)t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
-
-        timerText.text = minutes + ":" + seconds;
-        */
+        print("START");
+        isRunning = true;
+        startTime = Time.time;
     }
 
-    public void StopTimer()
+    public void TimerStop()
     {
-        print("STOP");
-        /*
-        timerText.text = stop
-        */
+        if (isRunning == true)
+        {
+            print("STOP");
+            isRunning = false;
+            stopTime = timerTime;
+        }
     }
 
+    public void TimerReset()
+    {
+        print("RESET");
+        timerMinutes.text = timerSeconds.text = timerMilliSeconds.text = "00";
+    }
+
+    private void Update()
+    {
+        timerTime = stopTime + (Time.time - startTime);
+        int minutesInt = (int)timerTime / 60;
+        int secondsInt = (int)timerTime % 60;
+        int milliSecondsInt = (int)(Mathf.Floor((timerTime - (secondsInt + minutesInt * 60)) * 100));
+
+        if (isRunning)
+        {
+            timerMinutes.text = (minutesInt < 10) ? "0" + minutesInt : minutesInt.ToString();
+            timerSeconds.text = (secondsInt < 10) ? "0" + secondsInt : secondsInt.ToString();
+            timerMilliSeconds.text = (milliSecondsInt < 10) ? "0" + milliSecondsInt : milliSecondsInt.ToString();
+        }
+    }
 }
